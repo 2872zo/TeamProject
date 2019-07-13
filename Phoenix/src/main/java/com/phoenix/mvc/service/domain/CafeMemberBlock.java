0c6ad6ae2026bb983d1394274fa9ee4d1c8ea962
John <John@John-PC>
2019-07-13 20:05:22 +0900

    kgh : cafeMemberBlock 누락된 거 추가

---
+++ Phoenix/src/main/java/com/phoenix/mvc/service/domain/CafeMemberBlock.java
@@ -0,0 +1,50 @@
+package com.phoenix.mvc.service.domain;
+
+import java.util.Date;
+
+public class CafeMemberBlock {
+	
+	private int memberNo;
+	private int blockNo;
+	private Date blockStartDate;
+	private Date blockEndDate;
+	private String blockReason;
+	
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public int getBlockNo() {
+		return blockNo;
+	}
+	public void setBlockNo(int blockNo) {
+		this.blockNo = blockNo;
+	}
+	public Date getBlockStartDate() {
+		return blockStartDate;
+	}
+	public void setBlockStartDate(Date blockStartDate) {
+		this.blockStartDate = blockStartDate;
+	}
+	public Date getBlockEndDate() {
+		return blockEndDate;
+	}
+	public void setBlockEndDate(Date blockEndDate) {
+		this.blockEndDate = blockEndDate;
+	}
+	public String getBlockReason() {
+		return blockReason;
+	}
+	public void setBlockReason(String blockReason) {
+		this.blockReason = blockReason;
+	}
+	
+	@Override
+	public String toString() {
+		return "CafeMemberBlock [memberNo=" + memberNo + ", blockNo=" + blockNo + ", blockStartDate=" + blockStartDate
+				+ ", blockEndDate=" + blockEndDate + ", blockReason=" + blockReason + "]";
+	}
+	
+}
