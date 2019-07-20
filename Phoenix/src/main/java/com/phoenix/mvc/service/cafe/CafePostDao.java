78750201e3293a6f632e4b1fe714a9bed9b0c2bf
home <home@DESKTOP-NP3D7LI>
2019-07-20 13:17:55 +0900

    YSG : template 추가

--- Phoenix/src/main/java/com/phoenix/mvc/service/cafe/CafePostDao.java
+++ Phoenix/src/main/java/com/phoenix/mvc/service/cafe/CafePostDao.java
@@ -59,4 +59,6 @@ public interface CafePostDao {
 	public List<Post> getPostListByMember(Search search);
 
 	public int memberPostTotalCount(Search search);
+
+	public List<Post> getBestPostList(int boardNo);
 }
