0cc7471603f3e3295010f1f0bbec9016ccde946e
user <user@DESKTOP-74IMAPF>
2019-08-01 20:52:46 +0900

    kgh : dd

--- Phoenix/src/main/resources/static/js/custom/chatRoomScript.js
+++ Phoenix/src/main/resources/static/js/custom/chatRoomScript.js
@@ -52,8 +52,6 @@ $("#updateChatRoomName").click(function() {
 								"방 제목이 " + inputValue+" 로 변경되었습니다.", "success");
 									},
 					error : function(request,status,error){
-										alert(request);
-										alert(status);
 								        alert("에러남 : "+error);
 								       }
 					}
@@ -79,13 +77,11 @@ $(".inviteList").on("click" , function() {
 });
 //초대하기 눌렀을 때
 $("#doInviting").on("click" , function() {
-
 	var jsonList = new Array();
 
 	var i = 0;
 	$(".inviteList").each(function(index) {
 		if($(this).css( "background-color" )=="rgb(245, 161, 66)"){
-			alert($("#chatRoomId").val());
 			var userNoJson = $(this).attr("id");
 			var nickNameJson = $(this).attr("name");
 			var profileImgJson = $(this).attr("value");
@@ -96,11 +92,7 @@ $("#doInviting").on("click" , function() {
 							profileImg : profileImgJson
 							};
 			
-			//beJson = JSON.stringify(beJson);
 			jsonList[i]=beJson;
-			//nickNameList[i]=$(this).attr("name");
-			//userNoList[i]=$(this).attr("id");
-			//imgFileList[i]=$(this).attr("value");
 			
 			i+=1;
 			$(this).attr("class","row d-flex justify-content-start");
@@ -112,10 +104,6 @@ $("#doInviting").on("click" , function() {
 		}
 	});
 	jsonList = JSON.stringify(jsonList);
-	//alert(jsonList);
-	//alert(nickNameList);
-	//alert(userNoList);
-	//alert(imgFileList);
 	
 	$.ajax(
 			{
@@ -125,14 +113,7 @@ $("#doInviting").on("click" , function() {
 			contentType: "application/json", //보내는 컨텐츠의 타입
 			//dataType : "json",      //받아올 데이터의 타입 필요없음
 			success : function(serverData, status) {
-								//alert(status);
-								//alert("server에서 온 Data : \n" + serverData);
-								//alert(serverData.regDate);
-								//targetTag.text(checker);	
-								//socket.emit("send_msg", $("#chatRoomNo").val()+":"+$("#msg").val()+serverData.regDate);
-								//socket.emit("send_msg", $("#chatRoomNo").val()+"-"+JSON.stringify(serverData));
-								//socket.emit("send_msg", serverData); 
-								// $("#msg").val("");
+				alert(i);
 							},
 			error : function(request,status,error){
 								alert(status);
@@ -145,8 +126,24 @@ $("#doInviting").on("click" , function() {
 
 //방 나가기
 $("#leaveChatRoom").on("click" , function() {
-	//$(self.location).attr("href","/chat/leaveChatRoom");
-	$("#thisChatRoomInfo").attr("method" , "POST").attr("action" , "/chat/leaveChatRoom").submit();
+	
+	swal({
+		  title: "나가실건가요?",
+		  text: "나가시면 되돌릴 수 없어요",
+		  type: "warning",
+		  showCancelButton: true,
+		  confirmButtonClass: "btn-success",
+		  confirmButtonText: "나갈거야!",
+		  cancelButtonText: "취소",
+		  closeOnConfirm: false
+		},
+		function(){
+		  swal("퇴장", "다음에 만나요", "success");
+		  setTimeout(function() {
+			  $("#thisChatRoomInfo").attr("method" , "POST").attr("action" , "/chat/leaveChatRoom").submit();
+			}, 1500);
+		});
+	
 });
 //이모티콘 입력
 $(".emoticons").on("click" , function() {
